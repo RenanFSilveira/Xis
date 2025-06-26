7b22bf597dbecaa3a979b83a4685e8472a4ecaf4
RenanFSilveira <146729229+RenanFSilveira@users.noreply.github.com>
2025-06-26 13:04:31 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Script from "next/script"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -7,9 +8,9 @@ export const metadata: Metadata = {
   description:
     "Curso profissionalizante em próteses odontológicas com certificação e metodologia exclusiva desenvolvida por Márcia Sales, especialista com mais de 35 anos de experiência.",
   keywords: "próteses odontológicas, curso profissionalizante, Vitória, ES, Márcia Sales, CAD/CAM, prótese dentária",
-    generator: 'v0.dev',
-    icons: {
-      icon: '/images/logo.png', // Caminho para a sua logo.png na pasta public/images
+  generator: 'v0.dev',
+  icons: {
+    icon: '/images/logo.png',
   },
 }
 
@@ -20,19 +21,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
-      {/* Aqui você pode substituir a fonte padrão por uma fonte customizada */}
-      {/* 
-        Para usar uma fonte customizada, você pode:
-        1. Adicionar um link para uma fonte do Google Fonts:
-        <head>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NXQWJBFN');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+        {/* 
+          Se quiser usar Google Fonts, adicione aqui:
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=SuaFonte:wght@400;500;700&display=swap" rel="stylesheet" />
-        </head>
-        
-        2. Ou usar uma fonte local com @font-face no globals.css
-      */}
-      <body className="antialiased">{children}</body>
+        */}
+      </head>
+      <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NXQWJBFN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
